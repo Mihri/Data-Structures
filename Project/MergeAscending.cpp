519f6256f5f17d55175cc1a49b0e97e6e31a317b
Mihri <mihribanguneydas@gmail.com>
2022-05-13 18:56:00 -0500

    Create MergeAscending.cpp

---
+++ Project/MergeAscending.cpp
@@ -0,0 +1,108 @@
+/*
+Author: Mihriban Guneydas
+Instructions:This code compiled and run on VS Code, with
+Please run this code on VS Code with C/C++ v1.7.1(by Microsoft), Code Runner v0.11.6(by Jun Han) extensions
+References: https://www.geeksforgeeks.org/merge-sort/*/
+
+#include <iostream>
+#include <fstream>
+#include <time.h>
+using namespace std;
+// C++ program for Merge Sort
+// Merges two subarrays of array[].
+// First subarray is arr[begin..mid]
+// Second subarray is arr[mid+1..end]
+void merge(int array[], int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+    // Create temp arrays
+    auto* leftArray = new int[subArrayOne],
+        * rightArray = new int[subArrayTwo];
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+    auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+        indexOfSubArrayTwo = 0; // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+// begin is for left index and end is
+// right index of the sub-array
+// of arr to be sorted */
+void mergeSort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return; // Returns recursively
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+// UTILITY FUNCTIONS
+// Function to print an array
+void printArray(int A[], int size)
+{
+    for (auto i = 0; i < size; i++)
+        cout << A[i] << " ";
+}
+// Driver code
+int main()
+{
+    string filename("AscendingOrder.txt");
+    int number;
+    int arr[100000];
+    int i = 0;
+    ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+            << filename << "'" << endl;
+        return EXIT_FAILURE;
+    }
+    while (input_file >> number) {
+        //cout << number << "; ";
+        arr[i] = number;
+        ++i;
+    }
+    cout << endl;
+    input_file.close();
+   // int arr[6] = { 12, 11, 13, 5, 6, 7 };
+    auto arr_size = sizeof(arr) / sizeof(arr[0]);
+    //cout << "Given array is \n";
+   // printArray(arr, 6);
+    clock_t start = clock();
+    mergeSort(arr, 0, 100000 - 1);
+    clock_t finish = clock();
+    double doneTime = double(finish - start) / double(CLOCKS_PER_SEC / 1000);
+    //cout << "\nSorted array is \n";
+    //printArray(arr, 100000);
+    cout <<endl<< doneTime*1000.0 << " milliseconds" << endl;
+    return 0;
+}
